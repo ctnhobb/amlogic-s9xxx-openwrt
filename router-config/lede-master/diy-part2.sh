aa2fc2b706d92485ace6b08d8cd9d0106bb7ce3d
ctnhobb <30974995+ctnhobb@users.noreply.github.com>
2022-03-08 23:16:11 +0700

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -39,8 +39,8 @@ svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passw
 svn co https://github.com/hubutui/p7zip-lede/trunk package/p7zip
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 
-find /home/runner/work/ -name passwall.po
-mv /home/runner/work/amlogic-s9xxx-openwrt/luci-app-passwall package/
+find /home/runner/work/ -name luci-app-amlogic
+mv /home/runner/work/amlogic-s9xxx-openwrt/amlogic-s9xxx-openwrt/luci-app-passwall package/
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
 # svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
